fb19c80a9df26761d018a0e14a8f175f839977d1
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2023-03-15 20:09:45 -0300

    New-Branch

--- script.js
+++ script.js
@@ -6,83 +6,85 @@ const ul = document.querySelector('ul')
 var itemsDB = []
 
 btnDeleteAll.onclick = () => {
-    itemsDB = []
-    updateDB()
+  itemsDB = []
+  updateDB()
 }
 
-
-texto.addEventListener('keypress', text => {
-    // console.log ('tecla pressionada')
-    if (text.key === 'Enter' && texto.value !== '') {
-        setItemDB()
-    }
+texto.addEventListener("keypress", e => {
+  // console.log ('tecla pressionada')
+  if (e.key === "Enter" && texto.value !== "") {
+    setItemDB()
+    updateDB()
+  }
 })
 
 btnInsert.onclick = () => {
-    //console.log ('aperta o botão direito')
-    if (texto.value !== '') {
-        setItemDB()
-    }
+  //console.log ('aperta o botão direito')
+  if (texto.value !== '') {
+    setItemDB()
+    updateDB()
+  }
 }
 function setItemDB() {
-    if (itemsDB.length >= 20) {
-        //console.log (alert)
-        alert('Limite máximo de 20 itens atingido!')
-        return
-    }
-    itemsDB.push({ 'item': texto.value, 'status': '' })
+  if (itemsDB.length >= 20) {
+    //console.log (alert)
+    alert("Limite máximo de 20 itens atingido!")
+    return
+  }
+  itemsDB.push({ item: texto.value, 'status': ''})
 }
 
 function updateDB() {
-    localStorage.setItem('todolist', JSON.stringify(itemsDB))
-    loadItems()
+  localStorage.setItem('todolist', JSON.stringify(itemsDB))
+  loadItems()
 }
 
-
-
 function loadItems() {
-    ul.innerHTML = "";
-    itemsDB = JSON.parse(localStorage, getItem('todoList')) ?? []
-    itemsDB.forEach((item, i) => {
-        insertItemTela(item.item, item.status, i)
-    })
+  ul.innerHTML = ""
+  itemsDB = JSON.parse(localStorage, getItem('todolist')) ?? []
+  itemsDB.forEach((item, i) => {
+    insertItemTela(item.item, item.status, i)
+  })
 }
 
 function insertItemTela(text, status, i) {
-    const li = document.createElement('li')
+  const li = document.createElement('li')
 
-    li.innerHTML = `
+  li.innerHTML = `
 <div class="divLi">
-    <input type = "checkbox" ${status} data-i = ${i} onchange="done(this, ${i});" />
+    <input type = "checkbox" ${status} data-i = ${i} onchange="done(this, ${i})" />
     <span data-si=${i}>${text}</span>
     <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
    </div>
    `
-    ul.appendChild(li)
+  ul.appendChild(li)
 
-    if (status) {
-        document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
-    } else {
-        document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
+  if (status) {
+    newFunction()
+  } else {
+    document.querySelector(`[data-si="${i}"]`).classList.remove("line-through")
+  }
+
+  texto.value = ''
+
+    function newFunction() {
+        document.querySelector(`[data-si="${i}"]`).classList.add("line-through")
     }
-
-    texto.value = ''
 }
 function done(chk, i) {
 
-    if (chk.checked) {
+  if (chk.checked) {
         itemsDB[i].status = 'checked'
-    } else {
-        itemsDB[i].status = ''
-    }
+  } else {
+    itemsDB[i].status = ''
+  }
 
-    updateDB()
+  updateDB()
 }
 
 function removeItem(i) {
-    itemsDB.splice(i, 1)
-    updateDB()
+  itemsDB.splice(i, 1)
+  updateDB()
 }
 
 loadItems()
-
